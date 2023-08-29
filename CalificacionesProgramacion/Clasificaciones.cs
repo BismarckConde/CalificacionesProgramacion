4d4ac9be1494bd05621e14de4ba4081417aecacd
SISTEMAS2-06 <SISTEMAS2-06@DESKTOP-2IANIKU>
2023-08-29 12:11:37 -0600

    Agregar archivos de proyecto.

---
+++ CalificacionesProgramacion/Clasificaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalificacionesProgramacion
+{
+    public class Clasificaciones
+    {
+        public string nombre { get; set; }
+        int[] notas = new int[3];
+        int i = 0;
+
+
+        static int[] calificaciones()
+        {
+            int[] notas = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" INGRESE LA NOTA (i+1):");
+                notas[i] = (int)Convert.ToDouble(Console.ReadLine());
+            }
+             return notas;
+
+        }
+        static double promedio(double[]notas)
+        {
+            double suma = 0;
+        }
+
+
+    }
+}
---
+++ CalificacionesProgramacion/frmCalificacion.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace CalificacionesProgramacion
+{
+    partial class frmCalificacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(".");
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(139, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CLASIFICACIONES DE PROGRMACION";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE DEL ESTUDIANTE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(273, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NOTA1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(344, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOTA2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(423, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "NOTA3";
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnregistrar.Location = new System.Drawing.Point(483, 98);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(90, 22);
+            this.btnregistrar.TabIndex = 5;
+            this.btnregistrar.Text = "REGISTRAR";
+            this.btnregistrar.UseVisualStyleBackColor = true;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(12, 97);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(237, 23);
+            this.txtnombre.TabIndex = 6;
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(273, 96);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(52, 23);
+            this.txtn1.TabIndex = 7;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(344, 97);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(52, 23);
+            this.txtn2.TabIndex = 8;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(423, 97);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(52, 23);
+            this.txtn3.TabIndex = 9;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            listViewItem1.IndentCount = 120;
+            listViewItem1.Tag = "";
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(12, 142);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(561, 268);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Estudiante";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nota 1";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Nota 2";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Nota 3";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Promedio";
+            this.columnHeader5.Width = 120;
+            // 
+            // frmCalificacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 424);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCalificacion";
+            this.Text = "frmCalificacion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button btnregistrar;
+        private TextBox txtnombre;
+        private TextBox txtn1;
+        private TextBox txtn2;
+        private TextBox txtn3;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+    }
+}
